b2db6ce8248e35c074ddd15c48a33d04ba30e62b
Daniel Méndez <daphni_mgld@hotmail.com>
2023-03-21 13:23:54 -0600

    Middleware for logs modified for more clarity

--- src/middleware/log.ts
+++ src/middleware/log.ts
@@ -1,8 +1,10 @@
 import { Request, Response, NextFunction } from "express";
 
 const logMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  console.log('Headers', req.headers);
-  console.log('Url', req.path);
+  console.log(`⚡️[server]: Request Path: ${JSON.stringify(req.path, null, 2)}`);
+  console.log(`⚡️[server]: Request Body: ${JSON.stringify(req.body, null, 2)}`);
+  console.log(`⚡️[server]: Request Query: ${JSON.stringify(req.query, null, 2)}`);
+  console.log(`⚡️[server]: Request Params: ${JSON.stringify(req.params, null, 2)}`);
 
   next();
 }
